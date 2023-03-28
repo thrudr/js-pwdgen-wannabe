fb0b85a5a790005ca0f2cb1f935808f8b7a515d6
thrudr <ariel.oliveiraesilva@gmail.com>
2023-03-28 16:03:41 +0200

    exercise-js-completed

---
+++ main.js
@@ -0,0 +1,7 @@
+const Name = prompt ("Qual'è il tuo nome?");
+//The variant "name" risulted -deprecated-.
+
+const surname = prompt ("E il tuo cognome?");
+const favoriteColor = prompt ("Ora dimmi il tuo colore preferito:");
+
+console.log(Name+surname+favoriteColor+21);
